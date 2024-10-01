fee595782332eb33c03e3b109ea10d083f271e95
mohamedtta <mkamed2@gmail.com>
2024-10-01 11:06:45 +0300

    Create app color file to put all the colors app on it

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color blueColor        = const Color(0xff34B7F1);
+  static Color lightGreen       = const Color(0xffDCF8C6);
+  static Color greenColor       = const Color(0xff25D366);
+  static Color darkGreenColor   = const Color(0xff128C7E);
+  static Color page             = const Color(0xffDCF8C6);
+}
